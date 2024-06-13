6afe31cf105f9920fad78d420dadee92546290a2
Prabhkirat Singh <Prabhkiratsingh2278@hmail.com>
2024-06-13 11:32:37 +0530

    changes

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -6,26 +6,26 @@ export default function Landing() {
 
    const token = localStorage.getItem('token')
    const navigate = useNavigate();
-   
+
    const navigateTo = () => {
 
-      
-      token ?  navigate('/dashboard') :  navigate('/signin');
-     
+
+      token ? navigate('/dashboard') : navigate('/signin');
+
    }
-return(
+   return (
 
-   <div className="flex flex-wrap flex-col justify-center items-center mt-20">
+      <div className="flex flex-wrap flex-col justify-center items-center mt-20">
 
-      <h1 className="text-center text-3xl  text-gray-900">Welcome to Lawyer Admin Dashboard</h1>
+         <h1 className="text-center text-3xl  text-gray-900">Welcome to Lawyer Admin Dashboard</h1>
 
-      { token ? <button onClick={ 
-                        navigateTo
-                     } className="bg-orange-500 text-white px-6 py-3 mt-5 w-fit rounded-lg  hover:bg-orange-600 transition-colors duration-300">{"Dashboard"} </button>
-                   : <button  onClick={ 
-                        navigateTo
-                     } className="bg-orange-500 text-white px-6 w-fit mt-5 py-3 rounded-lg  hover:bg-orange-600 transition-colors duration-300">{"Signin"} </button>
-                   }
-   </div>
-)
+         {token ? <button onClick={
+            navigateTo
+         } className="bg-orange-500 text-white px-6 py-3 mt-5 w-fit rounded-lg  hover:bg-orange-600 transition-colors duration-300">{"Dashboard"} </button>
+            : <button onClick={
+               navigateTo
+            } className="bg-orange-500 text-white px-6 w-fit mt-5 py-3 rounded-lg  hover:bg-orange-600 transition-colors duration-300">{"Signin"} </button>
+         }
+      </div>
+   )
 }

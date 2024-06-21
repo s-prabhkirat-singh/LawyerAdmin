3145f10a2de5ce4e5e7eaad8228c77613b384e88
Prabhkirat Singh <Prabhkiratsingh2278@hmail.com>
2024-06-21 11:06:37 +0530

    about page backend

--- backend-sequelize/routes/updateDataRoutes/aboutRoutes.js
+++ backend-sequelize/routes/updateDataRoutes/aboutRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const aboutController=require('../../controllers/updateDataControllers/aboutController')
 
-router.post('/addAboutData',aboutController.completeData)
+router.post('/addAboutData',aboutController.multiUpload,aboutController.updateAboutData)
 router.get('/getAboutData/:id',aboutController.getAboutDataById)
 
 

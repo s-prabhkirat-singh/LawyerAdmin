82968ab97d11eee86b637fdd706b0c07bd64bf52
Prabhkirat Singh <Prabhkiratsingh2278@hmail.com>
2024-06-24 11:03:20 +0530

    card section backend adde

--- backend-sequelize/routes/updateDataRoutes/aboutRoutes.js
+++ backend-sequelize/routes/updateDataRoutes/aboutRoutes.js
@@ -5,5 +5,12 @@ const aboutController=require('../../controllers/updateDataControllers/aboutCont
 router.post('/addAboutData',aboutController.multiUpload,aboutController.updateAboutData)
 router.get('/getAboutData/:id',aboutController.getAboutDataById)
 
+// Routes for section 2 
+
+router.post('/addCardData',aboutController.upload.single('icon'),aboutController.addCard)
+router.post('/deleteCard/:id',aboutController.deleteCard)
+router.post('/updateCardData',aboutController.upload.single('icon'),aboutController.updateCardData)
+router.get('/getCardList',aboutController.getCardList)
+
 
 module.exports=router;

af139d18658a259cf8a1dd45dc2baa75948bab26
Prabhkirat Singh <Prabhkiratsingh2278@hmail.com>
2024-06-25 11:00:53 +0530

    updates in card sectin and added faq backend files

---
+++ backend-sequelize/models/faq.js
@@ -0,0 +1,63 @@
+module.exports = (sequelize, DataTypes) => {
+    const  Faq= sequelize.define('YourModelName', {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      metatitle: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      metadescription: {
+        type: DataTypes.TEXT,
+        allowNull: true
+      },
+      metatags: {
+        type: DataTypes.TEXT,
+        allowNull: true
+      },
+      metaimage: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      headertitle: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      headerdescription: {
+        type: DataTypes.TEXT,
+        allowNull: true
+      },
+      headerbuttonlabel: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      headerbuttonlink: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      headerbgimage: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: true
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: true
+      }
+    }, {
+      tableName: 'YourTableName', // Match the existing MySQL table name
+      timestamps: false // Disable timestamps if the table doesn't have createdAt and updatedAt columns
+    });
+  
+    return Faq;
+  };
+  
---
+++ backend-sequelize/routes/updateDataRoutes/faqRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const faqController= require('../../controllers/updateDataControllers/faqController')
+
+
+router.post()
+
+
+
+
+module.exports=router;
